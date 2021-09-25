ecae757eae3cd12fccf484d9a116a1152c3e5d8e
Kris Tina <kristyguerrero20@gmail.com>
2021-09-25 10:03:48 -0500

    added components, and started styling

---
+++ kguerrero-portfolio/src/components/AboutMe.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <>
+      <section className="aboutMe">
+        <img
+          src="./assets/aboutMePic.jpg"
+          className="float-left"
+          alt="about-me-picture-of-Kristiel"
+        />
+        <h2 id="aboutMe">About Me</h2>
+        <p>
+          - Tarleton Alumni with a Bachelors in Science of Interdiscliplinary
+          Studies with Bilingual Supplement.
+          <br />
+          <br />
+          - Dual Language teacher in Fort Worth ISD
+          <br />
+          <br />
+          - Future Full-Stack Web Developer graduate from Southern Methodist
+          University
+          <br />
+          <br />- Music enthusiast, avid hiker, book-lover
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default AboutMe;
---
+++ kguerrero-portfolio/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <a href="#" id="top">
+        <h1>Kristiel Guerrero</h1>
+      </a>
+      <nav>
+        <ul>
+          <li>
+            <a href="#aboutMe">ABOUT ME</a>
+          </li>
+          <li>
+            <a href="#projects">PROJECTS</a>
+          </li>
+          <li>
+            <a href="#resume">RESUME</a>
+          </li>
+          <li>
+            <a href="#contact">CONTACT ME</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
